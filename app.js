7ea1073e5a55494701cbb48e9cc3bfd7c902c5c2
laurasantos2000 <laurasantos2000sss@gmail.com>
2025-09-23 22:44:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo(nome) {
+    if (nome && !amigos.includes(nome)) {
+        amigos.push(nome);
+        atualizarLista();
+    }
+}
